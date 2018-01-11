15d72991a8d9ea21b23d1181d04f275168634c54
rodrigo vega <rivega1707@gmail.com>
2018-01-10 18:28:26 -0800

    portfolio2

---
+++ main/black/projects/codePlayer/js/main.js
@@ -0,0 +1,26 @@
+function updatedOutput(){
+	$("iframe").contents().find("html").html("<html><head><style type='text/css'>" + $("#cssPanel").val() + "</style></head><body>" + $("#htmlPanel").val() + "</body></html>");
+	
+
+	document.getElementById("outputPanel").contentWindow.eval($("#javascriptPanel").val())
+}
+$(".toggleButton").hover(function(){
+	$(this).addClass("highlightedButton")
+}, function(){
+	$(this).removeClass("highlightedButton")
+})
+$(".toggleButton").click(function(){
+	$(this).toggleClass("active")
+	$(this).removeClass("highlightedButton")
+	var panelId = $(this).attr("id") + "Panel"
+	$("#" + panelId).toggleClass("hidden")
+	var numberofActivePanels = 4 - $(".hidden").length;
+	$(".panel").width(($(window).width() / numberofActivePanels) -10) 
+})
+
+$(".panel").height($(window).height() - $("#header").height() - 20)
+$(".panel").width(($(window).width() / 2) - 15)
+updatedOutput();
+$("textarea").on("change keyup paste", function(){
+	updatedOutput();
+})
